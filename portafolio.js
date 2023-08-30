e47a4d57ab1db605ec3e93e9a39bf6978c266e27
Gloria Villagran <glo.villagran@gmail.com>
2023-08-29 21:57:40 -0400

    formulario de contacto listo

--- portafolio.js
+++ portafolio.js
@@ -1,39 +1,51 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const sgMail = require('@sendgrid/mail');
+// Inicializa Email.js con tu User ID
+emailjs.init("qqxzrBl0eB4LL3m8j");
 
-const app = express();
+$("#submit-btn").click(function (e) {
+    e.preventDefault();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+    const name = $("#nombre").val();
+    const  email = $("#correo").val();
+    const message = $("#mensaje").val();
 
-// Configura SendGrid con tu API Key
-sgMail.setApiKey('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');
+    const alert = $("#alert");
+    if (name === "" || email === "" || message === "") {
+        $("#alert").html(`
+        <div class="alert alert-warning d-flex align-items-center" role="alert">
+  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+  <div>
+   Por favor completar los campos
+  </div>
+</div>`);
+        return
+    }
 
-// Ruta para manejar el envío del formulario
-app.post('/enviar-correo', (req, res) => {
-  const { nombre, correo, mensaje } = req.body;
+    const template_params = {
+        from_name: name,
+        from_email: email,
+        message: message
+    };
+   
+    // Utiliza el Service ID y el Template ID
+    emailjs.send("service_tr5k6z8", "template_pnk4pl9", template_params)
+        .then(function (response) {
+            $("#alert").html(`
+            <div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+             <div>
+                Mensaje enviado con exito
+             </div>
+            </div>
+            `)
 
-  const msg = {
-    to: 'glo.villagran@gmail.com', 
-    from: correo,
-    subject: 'Nuevo mensaje de contacto',
-    text: `Nombre: ${nombre}\nCorreo: ${correo}\nMensaje: ${mensaje}`
-  };
-
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Correo enviado');
-      res.status(200).send('Correo enviado con éxito');
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).send('Error al enviar el correo');
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
+        }, function (error) {
+            $("#alert").html(`
+            <div class="alert alert-danger d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+             <div>
+                 Error al enviar el mensaje
+             </div>
+            </div>
+            `)
+        });
 });
